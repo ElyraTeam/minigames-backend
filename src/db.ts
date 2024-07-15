bff2a88a06d6ae3efb0fbfc61d26f826335efe33
Hossam Mohsen <6911267+HossamMohsen7@users.noreply.github.com>
2024-07-15 03:51:06 +0300

    Fix types

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb";
 
-const db = new MongoClient(process.env.DATABASE_URL);
+const db = new MongoClient(process.env.DATABASE_URL!);
 
 const minigames_db = db.db("minigames");
 
