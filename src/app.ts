54d0040c00f31b9786993886aaf96383a7a52ca9
Hossam Mohsen <6911267+HossamMohsen7@users.noreply.github.com>
2026-01-06 02:06:04 +0200

    Update CORS options to allow all origins
    Simplified CORS configuration by allowing all origins.

--- src/app.ts
+++ src/app.ts
@@ -30,11 +30,8 @@ Sentry.init({
 });
 
 const corsOptions: cors.CorsOptions = {
-  origin: (origin, callback) => {
-    callback(null, origin);
-  },
-  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
+  origin: true,
 };
 
 storage.io = new Server(http, {
